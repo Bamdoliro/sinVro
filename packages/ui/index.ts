04f1a0df1cc4180047792ef2186594852646c8a7
junghongseop <a01082372487@gmail.com>
2024-08-26 08:06:06 +0900

    fix: 배경 절대경로를 위한 코드 삭제

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,5 +1,4 @@
 export { default as CustomText } from './src/Text/Text';
-export { default as Background } from './src/Background/Background';
 export { default as Button } from './src/Button/Button';
 export { default as Column } from './src/Flex/Column';
 export { default as Row } from './src/Flex/Row';
