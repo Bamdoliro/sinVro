ae5cd54f213afac7da945956c334ed1e22689a3a
junghongseop <a01082372487@gmail.com>
2024-09-02 19:30:57 +0900

    feat: 상세 감정 카드 개발

--- packages/ui/src/EmotionCard/DetailedCard.tsx
+++ packages/ui/src/EmotionCard/DetailedCard.tsx
@@ -10,12 +10,14 @@ interface Props {
   isFocused: boolean;
 }
 
-const { width: screenWidth } = Dimensions.get('window');
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 const iPhone13MiniWidth = 375;
+const iPhone13MiniHeight = 780;
 
-const calculateWidth = (baseWidth: number) => {
-  return (screenWidth / iPhone13MiniWidth) * baseWidth;
-};
+const calculateWidth = (baseWidth: number) =>
+  (screenWidth / iPhone13MiniWidth) * baseWidth;
+const calculateHeight = (baseHeight: number) =>
+  (screenHeight / iPhone13MiniHeight) * baseHeight;
 
 const DetailedCard = ({ onPress, children }: Omit<Props, 'isFocused'>) => {
   const [isFocused, setIsFocused] = useState<boolean>(false);
@@ -38,7 +40,7 @@ export default DetailedCard;
 
 const StyledDetailedCard = styled(Pressable)<{ isFocused: boolean }>`
   width: ${calculateWidth(159)}px;
-  height: ${calculateWidth(64)}px;
+  height: 64px;
   background-color: ${({ isFocused }: Props) =>
     isFocused ? color.white80 : color.glassWhite};
   border: 1px solid ${color.glassStroke};
