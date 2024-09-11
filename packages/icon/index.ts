62967128aba7d9d60a4839a43497651c7c35b4a3
junghongseop <a01082372487@gmail.com>
2024-09-12 00:16:51 +0900

    feat: 물음표 아이콘 컴포넌트 개발

--- packages/icon/index.ts
+++ packages/icon/index.ts
@@ -32,3 +32,5 @@ export { default as IconWriter2 } from './src/IconWriter2';
 export { default as IconPad1 } from './src/IconPad1';
 export { default as IconPad2 } from './src/IconPad2';
 export { default as IconNextBlackArrow } from './src/IconNextBlackArrow';
+export { default as IconQuestion1 } from './src/IconQuestion1';
+export { default as IconQuestion2 } from './src/IconQuestion2';
