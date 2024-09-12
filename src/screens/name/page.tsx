4b6e54eef9140e07362f9b9e2bf546dcaa92cb19
oxjadex <oharin765@gmail.com>
2024-09-12 17:51:52 +0900

    fix: 메시지 수정

--- src/screens/name/page.tsx
+++ src/screens/name/page.tsx
@@ -3,6 +3,7 @@ import { CustomText, Input, Button } from '@sinabro/ui';
 import styled from 'styled-components/native';
 import { color } from '@sinabro/design-token';
 import { calculateHeight, flex } from '@sinabro/util';
+import { IconWhiteArrow } from '@sinabro/icon';
 
 const NamePage = () => {
   const [isButtonVisible, setIsButtonVisible] = useState(false);
@@ -13,37 +14,51 @@ const NamePage = () => {
 
   return (
     <NameContainer>
-      <InputContainer>
-        <CustomText fontType="H4" color={color.white100}>
-          당신의 이름을 알려주세요.
-        </CustomText>
-        <Input
-          onChange={() => {}}
-          onSubmit={handleEnterPress}
-          placeholder="이름을 입력해주세요"
-        />
-      </InputContainer>
-      {isButtonVisible && (
-        <ButtonContainer>
-          <Button size="SMALL" icon="BLACKARROW_ICON" onPress={() => {}}>
-            다음으로
-          </Button>
-        </ButtonContainer>
-      )}
+      <IconContainer>
+        <IconWhiteArrow width={23} height={17} />
+      </IconContainer>
+      <ContentContainer>
+        <InputContainer>
+          <CustomText fontType="H4" color={color.white100}>
+            불리고 싶은 이름을 알려주세요.
+          </CustomText>
+          <Input
+            onChange={() => {}}
+            onSubmit={handleEnterPress}
+            placeholder="이름을 입력해주세요"
+          />
+        </InputContainer>
+        {isButtonVisible && (
+          <ButtonContainer>
+            <Button size="SMALL" icon="BLACKARROW_ICON" onPress={() => {}}>
+              다음으로
+            </Button>
+          </ButtonContainer>
+        )}
+      </ContentContainer>
     </NameContainer>
   );
 };
 
 const NameContainer = styled.View`
   background-color: ${color.gray900};
+  gap: ${calculateHeight(10)}px;
   width: 100%;
   height: 100%;
-  gap: ${calculateHeight(320)}px;
+`;
+
+const ContentContainer = styled.View`
+  gap: ${calculateHeight(340)}px;
+`;
+
+const IconContainer = styled.View`
+  padding-top: ${calculateHeight(72)}px;
+  padding-left: ${calculateHeight(20)}px;
 `;
 
 const InputContainer = styled.View`
   ${flex({ alignItems: 'center', justifyContent: 'flex-start' })}
-  padding-top: ${calculateHeight(224)}px;
+  padding-top: ${calculateHeight(120)}px;
   text-align: center;
   gap: 16px;
 `;
