8e5fa66cc01fcd33c110ade296a33b4242303ffe
oxjadex <oharin765@gmail.com>
2024-09-20 21:04:02 +0900

    fix: 초이스 애니메이션 추가

--- packages/ui/src/Choices/Choices.tsx
+++ packages/ui/src/Choices/Choices.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components/native';
-import { Pressable } from 'react-native';
+import { Pressable, Animated } from 'react-native';
 import { color } from '@sinabro/design-token';
 import Row from '../Flex/Row';
 import CustomText from '../Text/Text';
@@ -14,9 +14,23 @@ type Props = {
 
 const Choices = ({ onPress, children, isDisabled }: Props) => {
   const [isFocused, setIsFocused] = useState<boolean>(false);
+  const animatedValue = useRef(new Animated.Value(0)).current;
 
   const handlePress = () => {
     if (isDisabled) return;
+
+    Animated.timing(animatedValue, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(animatedValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }).start(()=> {setIsFocused(false);});
+    });
+
     setIsFocused(true);
     onPress();
 
@@ -25,26 +39,32 @@ const Choices = ({ onPress, children, isDisabled }: Props) => {
     }, 1000);
   };
 
+  const backgroundColor = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['transparent', color.primary],
+  });
+
   return (
-    <StyledButton onPress={handlePress} isFocused={isFocused}>
-      <Row alignItems="center" justifyContent="center">
-        <CustomText fontType="B3" color={isFocused ? color.gray900 : color.white100}>
-          {children}
-        </CustomText>
-      </Row>
-    </StyledButton>
+    <Pressable onPress={handlePress}>
+      <Animated.View style={{ backgroundColor }}>
+        <StyledButton>
+          <Row alignItems="center" justifyContent="center">
+            <CustomText fontType="B3" color={isFocused ? color.gray900 : color.white100}>
+              {children}
+            </CustomText>
+          </Row>
+        </StyledButton>
+      </Animated.View>
+    </Pressable>
   );
 };
 
-const StyledButton = styled(Pressable)<{ isFocused: boolean }>`
+const StyledButton = styled.View`
   height: ${calculateHeight(200)}px;
   width: ${calculateWidth(160)}px;
-
   border-color: ${color.primary};
   border-width: 1px;
   border-style: solid;
-  background-color: ${(props: { isFocused: any }) =>
-    props.isFocused ? color.primary : 'transparent'};
   border-radius: 6px;
   justify-content: center;
 `;
