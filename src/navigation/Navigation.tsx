55681d55a7cbf3f411df0aa77d005a316ce0949d
oxjadex <oharin765@gmail.com>
2024-09-21 23:44:57 +0900

    feat: navigation 세팅

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StatusBar, View, SafeAreaView } from 'react-native';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -10,6 +9,14 @@ import NamePage from 'screens/NameInput/page';
 import PsychologicalTest from 'screens/PsychologicalTest/page';
 import SelectCharacterPage from 'screens/SelectCharacter/page';
 import StoryPage from 'screens/Story/page';
+import MyPage from 'screens/MyPage/page';
+import InquiryPage from 'screens/Inquiry/page';
+import InquiryDetailPage from 'screens/Inquiry/Detail';
+import SubmitInquiryPage from 'screens/Inquiry/SubmitInquiry';
+import SettingPage from 'screens/Setting/page';
+import FAQPage from 'screens/FAQ/page';
+import AlarmPage from 'screens/Alarm/page';
+
 import { color } from '@sinabro/design-token';
 
 const Stack = createStackNavigator();
@@ -66,6 +73,59 @@ const Navigation = () => {
             component={StoryPage}
             options={{
               header: () => <HeaderPage backgroundColor={color.gray900} />,
+            }}
+          />
+          <Stack.Screen
+            name="MyPage"
+            component={MyPage}
+            options={{
+              header: () => <HeaderPage backgroundColor={color.gray900} />,
+            }}
+          />
+          <Stack.Screen
+            name="Inquiry"
+            component={InquiryPage}
+            options={{
+              header: () => (
+                <HeaderPage backgroundColor={color.gray900} title="문의 내역" />
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="InquiryDetail"
+            component={InquiryDetailPage}
+            options={{
+              header: () => <HeaderPage backgroundColor={color.gray900} title="문의" />,
+            }}
+          />
+          <Stack.Screen
+            name="SubmitInquiry"
+            component={SubmitInquiryPage}
+            options={{
+              header: () => <HeaderPage backgroundColor={color.gray900} title="문의" />,
+            }}
+          />
+          <Stack.Screen
+            name="Setting"
+            component={SettingPage}
+            options={{
+              header: () => <HeaderPage backgroundColor={color.gray900} title="설정" />,
+            }}
+          />
+          <Stack.Screen
+            name="FAQ"
+            component={FAQPage}
+            options={{
+              header: () => (
+                <HeaderPage backgroundColor={color.gray900} title="자주 묻는 질문" />
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="Alarm"
+            component={AlarmPage}
+            options={{
+              header: () => <HeaderPage backgroundColor={color.gray900} title="알림" />,
             }}
           />
         </Stack.Navigator>
