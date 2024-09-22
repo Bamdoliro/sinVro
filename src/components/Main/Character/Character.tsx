5907958fdf7443fbb93a8fb58c01f402b1ea57dc
junghongseop <a01082372487@gmail.com>
2024-09-22 23:57:46 +0900

    feat: 캐릭터를 누르면 말풍선이 바뀌도록 개발

--- src/components/Main/Character/Character.tsx
+++ src/components/Main/Character/Character.tsx
@@ -1,14 +1,35 @@
+import React, { useState } from 'react';
 import { IconHeon } from '@sinabro/icon';
 import { CharacterChat, Column } from '@sinabro/ui';
-import React from 'react';
 import styled from 'styled-components/native';
+import { Pressable } from 'react-native';
 
 const Character = () => {
+  const context = [
+    '너를 만나 내 일상이 다채로워졌어',
+    '안녕?',
+    '반가워',
+    '오늘은 무슨 일이 있었어?',
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleIconPress = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % context.length);
+  };
+
   return (
     <StyledCharcter>
       <Column alignItems="center" gap={20}>
-        <CharacterChat>너를 만나 내 일상이 다채로워졌어</CharacterChat>
-        <IconHeon width={200} height={181.27} />
+        <CharacterChat>{context[currentIndex]}</CharacterChat>
+        <Pressable
+          onPress={handleIconPress}
+          style={({ pressed }) => ({
+            transform: pressed ? [{ scale: 0.95 }] : [{ scale: 1 }],
+          })}
+        >
+          <IconHeon width={200} height={181.27} />
+        </Pressable>
       </Column>
     </StyledCharcter>
   );
