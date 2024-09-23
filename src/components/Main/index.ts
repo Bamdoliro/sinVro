bfef619002ea321e508e36df6b100cf66ec5f77e
junghongseop <a01082372487@gmail.com>
2024-09-23 16:33:25 +0900

    chore: 컴포넌트 폴더 구조 수정으로 index.ts 수정

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -1,5 +1,7 @@
 export { default as Header } from './Header/Header';
 export { default as CategoryList } from './CategoryList/CategoryList';
 export { default as Character } from './Character/Character';
-export { default as TabBar } from './TabBar/TabBar';
 export { default as Title } from './Title/Title';
+export { default as DiaryTab } from './DiaryTab/DiaryTab';
+export { default as MailBoxTab } from './MailBoxTab/MailBoxTab';
+export { default as RadioTab } from './RadioTab/ReadioTab';
