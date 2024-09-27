0562342aaf02722810dfafb198c9c76680724dd9
junghongseop <a01082372487@gmail.com>
2024-09-27 14:41:39 +0900

    feat: 마이페이지 컴포넌트 개발

---
+++ src/components/MyPage/index.ts
@@ -0,0 +1,5 @@
+export { default as Name } from './Name/Name';
+export { default as SettingButton } from './SettingButton/SettingButton';
+export { default as QuestionButton } from './QuestionButton/QuestionButton';
+export { default as FAQButton } from './FAQButton/FAQButton';
+export { default as LoginPage } from './LogOutButton/LogOutButton';
