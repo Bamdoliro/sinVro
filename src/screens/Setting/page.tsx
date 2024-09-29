fed02544939bd4eb73bb2f176cdf7bdf42d74a4e
junghongseop <a01082372487@gmail.com>
2024-09-29 15:18:10 +0900

    fix: 세팅페이지 컴포넌트 분할 및 헤더 적용

--- src/screens/Setting/page.tsx
+++ src/screens/Setting/page.tsx
@@ -1,33 +1,15 @@
-import React, { useState } from 'react';
-import { CustomText, Toggle } from '@sinabro/ui';
+import React from 'react';
 import styled from 'styled-components/native';
 import { color } from '@sinabro/design-token';
-import { calculateHeight, calculateWidth, flex } from '@sinabro/util';
+import { calculateHeight } from '@sinabro/util';
+import { Header } from 'components/common';
+import { Alarm } from 'components/Setting';
 
 const SettingPage = () => {
-  const [onToggle, setOnToggle] = useState(true);
-  const handleToggle = () => {
-    setOnToggle(!onToggle);
-  };
-
   return (
     <StyledSetting>
-      <CustomText fontType="B4" color={color.white100}>
-        푸시 알림
-      </CustomText>
-      <AlarmContainer>
-        <TextContainer>
-          <CustomText fontType="B4" color={color.white100}>
-            야간 알림
-          </CustomText>
-          <CustomText fontType="B6" color={color.white100}>
-            (오후 12시 ~ 오전 7시)
-          </CustomText>
-        </TextContainer>
-        <ToggleContainer>
-          <Toggle onToggle={handleToggle} isOn={onToggle}></Toggle>
-        </ToggleContainer>
-      </AlarmContainer>
+      <Header title="설정" />
+      <Alarm />
     </StyledSetting>
   );
 };
@@ -37,30 +19,5 @@ const StyledSetting = styled.View`
   background-color: ${color.gray900};
   width: 100%;
   height: 100%;
-  padding: ${calculateHeight(40)}px ${calculateWidth(20)}px ${calculateHeight(102)}px
-    ${calculateWidth(20)}px;
-  gap: ${calculateHeight(14)}px;
-`;
-
-const AlarmContainer = styled.View`
-  position: relative;
-  ${flex({ justifyContent: 'space-between' })};
-  width: 100%;
-  height: ${calculateHeight(77)}px;
-  background-color: ${color.glassWhite};
-  border: 1px solid ${color.glassStroke};
-  border-radius: 8px;
-`;
-
-const TextContainer = styled.View`
-  position: relative;
-  ${flex({ flexDirection: 'column' })};
-  padding: ${calculateHeight(16)}px ${calculateWidth(12)}px;
-  border-radius: 6px;
-`;
-
-const ToggleContainer = styled.View`
-  position: absolute;
-  bottom: ${calculateHeight(26.5)}px;
-  right: ${calculateWidth(16)}px;
+  padding-top: ${calculateHeight(139)}px;
 `;
