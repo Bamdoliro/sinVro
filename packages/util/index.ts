5184af47370c9f58cca1cb2b17715557af2e4086
junghongseop <a01082372487@gmail.com>
2024-11-22 14:52:44 +0900

    fix(packages): 불필요한 util 파일 삭제

--- packages/util/index.ts
+++ packages/util/index.ts
@@ -2,4 +2,3 @@ export { default as flex } from './styles/flex';
 export { default as showToast } from './styles/showToast';
 export { default as calculateHeight } from './styles/calculateHeight';
 export { default as calculateWidth } from './styles/calculateWidth';
-export * from './stores/name';
