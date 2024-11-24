342d51e699511aa3fecb2cca3109be8bbc5ca683
junghongseop <a01082372487@gmail.com>
2024-11-24 23:32:50 +0900

    feat: api 연결한 값이 나오도록 수정

--- src/screens/FAQ/page.tsx
+++ src/screens/FAQ/page.tsx
@@ -6,87 +6,32 @@ import { color } from '@sinabro/design-token';
 import { calculateHeight, calculateWidth, flex } from '@sinabro/util';
 import { IconInequality } from '@sinabro/icon';
 import { Header } from 'components/common';
+import { useFaqListQuery } from 'services/faq/quries';
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
   UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
-interface Questions {
-  id: number;
-  question: string;
-  content: string;
-}
-
-const faqQuestions: Questions[] = [
-  {
-    id: 1,
-    question: '밤돌이로 자묻질 좀 채워주세요',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 2,
-    question: '캐릭터 말풍선 안 보임',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 3,
-    question: '버그나서 캐릭터가 뒤집어졌어요',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 4,
-    question: '일기가 매일 와서 부담스러워요',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 5,
-    question: '편지가 한 달 째 안와요',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 6,
-    question: '헌이가 답장을 안함',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 7,
-    question: '새벽에 편지가 와요. 잠을 안 자는 듯',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 8,
-    question: '편지가 한 달 째 안와요',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 9,
-    question: '헌이가 답장을 안함',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-  {
-    id: 10,
-    question: '새벽에 편지가 와요. 잠을 안 자는 듯',
-    content: '캐릭터들의 편지는 보통 1일~3일 내에 1통 발송됩니다',
-  },
-];
-
 const FAQPage = () => {
+  const { data: faqList } = useFaqListQuery();
   const [selectedQuestionId, setSelectedQuestionId] = React.useState<number | null>(null);
-  const animatedValues = useRef(faqQuestions.map(() => new Animated.Value(0))).current;
+  const animatedValues = useRef(faqList.map(() => new Animated.Value(0))).current;
 
   const handleToggleQuestion = (id: number) => {
-    const index = faqQuestions.findIndex((question) => question.id === id);
+    const index = faqList.findIndex((question) => question.id === id);
     const isSelected = selectedQuestionId === id;
-
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
 
     setSelectedQuestionId(isSelected ? null : id);
 
-    Animated.timing(animatedValues[index], {
-      toValue: isSelected ? 0 : 1,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
+    setTimeout(() => {
+      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+      Animated.timing(animatedValues[index], {
+        toValue: isSelected ? 0 : 1,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+    }, 0);
   };
 
   return (
@@ -94,7 +39,7 @@ const FAQPage = () => {
       <Header title="자주 묻는 질문" />
       <ScrollViewContainer>
         <ScrollView contentContainerStyle={{ paddingBottom: calculateHeight(0) }}>
-          {faqQuestions.map((question, index) => {
+          {faqList.map((question, index) => {
             const height = animatedValues[index].interpolate({
               inputRange: [0, 1],
               outputRange: [0, calculateHeight(77)],
@@ -111,21 +56,19 @@ const FAQPage = () => {
               >
                 <QuestionContainer>
                   <CustomText fontType="B4" color={color.white100}>
-                    {question.question}
+                    {question.title}
                   </CustomText>
-                  <IconContainer rotate={selectedQuestionId === question.id}>
-                    <IconInequality width={11} height={20}></IconInequality>
+                  <IconContainer $rotate={selectedQuestionId === question.id}>
+                    <IconInequality width={11} height={20} />
                   </IconContainer>
                 </QuestionContainer>
-                {selectedQuestionId === question.id && (
-                  <Animated.View style={{ height, opacity, overflow: 'hidden' }}>
-                    <ContentContainer>
-                      <CustomText fontType="B5" color={color.gray700}>
-                        {question.content}
-                      </CustomText>
-                    </ContentContainer>
-                  </Animated.View>
-                )}
+                <Animated.View style={{ height, opacity, overflow: 'hidden' }}>
+                  <ContentContainer>
+                    <CustomText fontType="B5" color={color.gray700}>
+                      {question.content}
+                    </CustomText>
+                  </ContentContainer>
+                </Animated.View>
               </FAQContainer>
             );
           })}
@@ -165,7 +108,7 @@ const QuestionContainer = styled.View`
 `;
 
 const IconContainer = styled.View<{ $rotate: boolean }>`
-  transform: rotate(${(props: any) => (props.rotate ? '90deg' : '270deg')});
+  transform: rotate(${(props: any) => (props.$rotate ? '90deg' : '270deg')});
 `;
 
 const ContentContainer = styled.View`
