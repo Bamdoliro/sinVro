0cc179a6787fe7866384c6ba09ead713184c5551
junghongseop <a01082372487@gmail.com>
2024-11-27 21:05:41 +0900

    fix: index 파일 추가

---
+++ src/components/onBoarding/index.ts
@@ -0,0 +1,4 @@
+export { default as EmailInput } from './login/input/email';
+export { default as PasswordInput } from './login/input/password';
+export { default as LoginButton } from './login/button/login';
+export { default as SignInButton } from './login/button/signin';
