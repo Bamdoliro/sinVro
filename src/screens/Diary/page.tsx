bc916f48611f57f8f39d6c86b0c635009d87b21b
junghongseop <a01082372487@gmail.com>
2024-12-03 08:10:15 +0900

    style: 일기 헤더 스타일 수정

--- src/screens/Diary/page.tsx
+++ src/screens/Diary/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { color } from '@sinabro/design-token';
 import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
-import { calculateHeight, flex } from '@sinabro/util';
+import { calculateHeight, calculateWidth, flex } from '@sinabro/util';
 import { IconNote, IconWhiteArrow } from '@sinabro/icon';
-import { Calender, Column, CustomText } from '@sinabro/ui';
+import { Calender, Column, CustomText, Row } from '@sinabro/ui';
 import { useNavigation } from '@react-navigation/native';
 import { useCharacterQuery } from 'services/character/quries';
 import { useDiaryListQuery } from 'services/diary/quries';
@@ -37,21 +37,23 @@ const DiaryPage = () => {
       }
     >
       <StyledHeader>
-        <IconContainer
-          onPress={() => {
-            navigation.reset({
-              index: 0,
-              routes: [{ name: 'Main' as never }],
-            });
-          }}
-        >
-          <IconWhiteArrow width={23} height={17} />
-        </IconContainer>
-        <TitleContainer>
-          <CustomText fontType="H3" color={color.white100}>
-            하루 일기
-          </CustomText>
-        </TitleContainer>
+        <RowContainer>
+          <IconWhiteContainer
+            onPress={() => {
+              navigation.reset({
+                index: 0,
+                routes: [{ name: 'Main' as never }],
+              });
+            }}
+          >
+            <IconWhiteArrow width={23} height={17} />
+          </IconWhiteContainer>
+          <TitleContainer>
+            <CustomText fontType="H3" color={color.white100}>
+              하루 일기
+            </CustomText>
+          </TitleContainer>
+        </RowContainer>
       </StyledHeader>
       <ContentContainer>
         <Column alignItems="center" gap={22}>
@@ -68,7 +70,6 @@ const DiaryPage = () => {
                     ? parseInt(selectedDiary.id, 10)
                     : selectedDiary.id;
                 handleDiaryClick(id);
-              } else {
               }
             }}
           />
@@ -84,6 +85,34 @@ const StyledDiaryPage = styled(LinearGradient)`
   flex: 1;
 `;
 
+const StyledHeader = styled.View`
+  position: absolute;
+  top: ${calculateHeight(67)}px;
+  left: 0;
+  width: 100%;
+  height: ${calculateHeight(50)}px;
+  z-index: 10;
+  background-color: transparent;
+`;
+
+const RowContainer = styled(Row)`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+`;
+
+const IconWhiteContainer = styled.TouchableOpacity`
+  position: absolute;
+  left: ${calculateWidth(20)}px;
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+`;
+
+const TitleContainer = styled.View`
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+  flex: 1;
+`;
+
 const ContentContainer = styled.View`
   ${flex({ alignItems: 'center' })}
   width: 100%;
@@ -95,18 +124,4 @@ const ContentContainer = styled.View`
 const IconContainer = styled.TouchableOpacity`
   margin-left: 134px;
   margin-right: 101px;
-`;
-
-const StyledHeader = styled.View`
-  position: absolute;
-  top: ${calculateHeight(67)}px;
-  left: 0;
-  width: 100%;
-  height: 40px;
-  z-index: 10;
-`;
-
-const TitleContainer = styled.View`
-  ${flex({ alignItems: 'center', justifyContent: 'center' })}
-  flex: 1;
 `;
